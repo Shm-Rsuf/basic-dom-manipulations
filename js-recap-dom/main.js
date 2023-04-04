7ee4239a736dc595ee0c339493181c2d6bd5dd70
shm_usuf <shmusuf@gmail.com>
2023-04-04 20:45:46 +0600

    element selector added

--- js-recap-dom/main.js
+++ js-recap-dom/main.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+/* // document.getElementById("id")
+const h1 = document.getElementById("headLine");
+// console.log(h1);
+
+// document.getElementsByClassName("className")
+const h2 = document.getElementsByClassName("headLine");
+// console.log(h2);
+
+// document.querySelector(selector)
+const head1 = document.querySelector("#headLine");
+console.log(head1);
+
+// document.querySelectorAll(selector)
+const head2 = document.querySelectorAll(".headLine");
+console.log(head2);
+ */
